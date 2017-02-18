c1b63b02cdbc0232d9e9de19d01274af4e2b33d9
Nguyen Manh Tung <nguyenmanhtung848@gmail.com>
2017-02-18 16:46:23 +0700

    include axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,12 @@
-const API_KEY = '9d11c30aca1aeac7c4570f623c1db6e1'
+const API_KEY = '9d11c30aca1aeac7c4570f623c1db6e1';
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url =`${ROOT_URL}&q=${city},us`;
+
+    return {
+        type: FETCH_WEATHER
+    };
+}
