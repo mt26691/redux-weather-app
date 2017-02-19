6e0b30ef585d0269ec704c7fb86f3ac2532e0a05
Nguyen Manh Tung <nguyenmanhtung848@gmail.com>
2017-02-19 09:59:18 +0700

    integate GoogleMap

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
 import { fetchWeather } from '../actions/index';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
@@ -15,10 +16,11 @@ class WeatherList extends Component {
         const temps = cityData.list.map(weather => weather.main.temp - 273);
         const pressures = cityData.list.map(weather => weather.main.pressure);
         const humidities = cityData.list.map(weather => weather.main.humidity);
+        const {lon, lat} = cityData.city.coord;
 
         return (
             <tr key={name}>
-                <td>{name}</td>
+                <td><GoogleMap lon={lon} lat={lat} /></td>
                 <td>
                     <Chart data={temps} color="orange" units="Celsius degree" />
                 </td>
